89626bb0fe5a14db48819089917ca9c0c6c309e6
d.naoto <naotolog@gmail.com>
2025-08-03 11:38:50 +0900

    記事ページのタイトルを修正し、アポストロフィをHTMLエンティティに置き換えました。また、未使用の矢印アイコンコンポーネントを削除しました。

--- src/app/bk_page.tsx
+++ src/app/bk_page.tsx
@@ -275,7 +275,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            👋 Hello, I'm Naoto
+            👋 Hello, I&apos;m Naoto
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I enjoy personal development and have built many apps. I would like to introduce some of them.
